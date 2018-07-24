d3b41ce2a502969702988d69f5553660eca4e518
Mandi Karpo <mandi.karpo@gmail.com>
2018-07-24 17:47:57 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -15,7 +15,16 @@ def curfew_checker(time)
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+  time = 20
+  if time >= 11
+    puts "You're in trouble! Better get home quick!"
+  elsif time < 11
+    puts "Keep having fun!"
+    elsif time == 11
+      puts "time = 20
+
+  end
+  end
 end
 
 def deluxe_curfew_checker(time)
